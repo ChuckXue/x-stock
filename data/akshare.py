90b5b3d4413e082e044b244039246a58b1c53f67
xuechengeng <xuechengeng@hzwtech.com>
2023-04-26 22:53:21 +0800

    配置项目结构

---
+++ data/akshare.py
@@ -0,0 +1,9 @@
+
+class AkShare:
+    """_summary_
+    文档: https://akshare.akfamily.xyz/introduction.html
+    更新: pip install akshare --upgrade -i https://pypi.org/simple
+
+    """
+    def __init__(self) -> None:
+        pass
